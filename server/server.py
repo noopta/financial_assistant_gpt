69c6a5e940259bf667205785fded50beea6193e4
Ubuntu <ubuntu@ip-172-31-16-182.us-east-2.compute.internal>
2023-11-21 03:11:38 +0000

    adding endpoints

--- server/server.py
+++ server/server.py
@@ -2,6 +2,9 @@ import time
 from openai import OpenAI
 import os
 import boto3
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 OpenAI.api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI()
@@ -72,55 +75,65 @@ def uploadFilesToAssistant():
 readFromS3()
 uploadFilesToAssistant()
 
-while(True):
-    threadMessage = input("What question about travelling did you have? No questions? Enter Q to exit.\n")
-    
-    if threadMessage == "Q" or threadMessage == "q":
-        break
-
-    message = client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role="user",
-        content=threadMessage
-    )
-
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id="asst_EwyAYD7GsbNhWzO3UeUG78kA",
-        instructions="Please address the user as Jane Doe. The user has a premium account."
-    )
-    
-    run = client.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id
-    )
-
-    time.sleep(20)
-
-    messages = client.beta.threads.messages.list(
-        thread_id=thread.id
-    )
-
-    # Iterate through the messages to find the assistant's response
-    assistant_response = None
-    for message in messages:
-        if message.role == "assistant":
-            assistant_response = message.content
+def askAssistant():
+    while(True):
+        threadMessage = input("What question about travelling did you have? No questions? Enter Q to exit.\n")
+        
+        if threadMessage == "Q" or threadMessage == "q":
             break
 
-    # Assuming you have the assistant_response as previously retrieved
-    if assistant_response:
-        # Check if the response is a list and not empty
-        if isinstance(assistant_response, list) and assistant_response:
-            # Access the text attribute of the first response
-            message_content = assistant_response[0].text
-            # Now access the value attribute of the text object
-            response_text = message_content.value
-            print("\n\n" + response_text)
-        else:
-            print("Response is not in the expected list format.")
-    else:
-        print("No response from the assistant found.")
+        message = client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=threadMessage
+        )
 
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id="asst_EwyAYD7GsbNhWzO3UeUG78kA",
+            instructions="Please address the user as Jane Doe. The user has a premium account."
+        )
+        
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+        time.sleep(20)
+
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+
+        # Iterate through the messages to find the assistant's response
+        assistant_response = None
+        for message in messages:
+            if message.role == "assistant":
+                assistant_response = message.content
+                break
+
+        # Assuming you have the assistant_response as previously retrieved
+        if assistant_response:
+            # Check if the response is a list and not empty
+            if isinstance(assistant_response, list) and assistant_response:
+                # Access the text attribute of the first response
+                message_content = assistant_response[0].text
+                # Now access the value attribute of the text object
+                response_text = message_content.value
+                print("\n\n" + response_text)
+            else:
+                print("Response is not in the expected list format.")
+        else:
+            print("No response from the assistant found.")
+
+# create a function that parses the response from the front end
+
+@app.route('/')
 def parseFrontEndResponse():
-    print("yo")
+    return "yo"
+
+@app.route('/post-endpoint', methods=['POST'])
+def handle_post():
+    data = request.json
+    print("Received data:", data)
+    return jsonify({"message": "Data received successfully!"})
