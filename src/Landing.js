7957f682a816540b40a0bef5226f9811389da518
Ubuntu <ubuntu@ip-172-31-18-109.us-east-2.compute.internal>
2024-02-12 02:30:16 +0000

    chat

--- src/Landing.js
+++ src/Landing.js
@@ -157,12 +157,14 @@ export default function Landing() {
                                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                         }}
                     />
+                    {/* Certificate is saved at: /etc/letsencrypt/live/docusync.ai/fullchain.pem
+Key is saved at:         /etc/letsencrypt/live/docusync.ai/privkey.pem */}
                 </div>
                 <div className="py-24 sm:py-32 lg:pb-40">
                     <div className="mx-auto max-w-7xl px-6 lg:px-8">
                         <div className="mx-auto max-w-2xl text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                                Your document analyst and storage system.
+                                Your documenttt analyst and storage system!
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-300">
                                 DocuSync is an interactive chatbot that helps you analyze, compare, contrast, and derive insights from querying agsint multiple documents such as textbooks, financial earnings, resumes, and more.
