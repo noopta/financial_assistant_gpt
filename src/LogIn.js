8ad2a9e85101bfd760f2d62fab4412be16fb6b80
Anupta Islam <anuptaislam33@gmail.com>
2024-02-25 18:31:05 -0500

    remove logo

--- src/LogIn.js
+++ src/LogIn.js
@@ -559,12 +559,12 @@ export default function LogIn() {
 
                 <div className="  flex min-h-full flex-1 flex-col justify-center px-6 py-4 lg:px-8">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                        <img
+                        {/* <img
                             className="mx-auto h-10 w-auto"
                             // src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                             src={Logo}
                             alt="Your Company"
-                        />
+                        /> */}
                         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
                             Sign in to your account
                         </h2>
